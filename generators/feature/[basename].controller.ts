af96140fbf268f764144bef736afc5882b054e83
unbywyd <unbywyd@gmail.com>
2025-03-02 11:16:20 +0200

    +

--- generators/feature/[basename].controller.ts
+++ generators/feature/[basename].controller.ts
@@ -6,14 +6,14 @@ import {
 import { Service } from "typedi";
 import { OpenAPI } from "routing-controllers-openapi";
 import { SuccessResponse, Summary } from '@tsdiapi/server';
-import {{className}}Service, { InputVerifyDTO, OutputSignInEmailDTO, OutputSignInPhoneDTO, OutputVerifyDTO, SignInEmailDTO, SignInPhoneDTO } from "./{{kebabcase}}.service";
+import {{className}}Service, { InputVerifyDTO, OutputSignInEmailDTO, OutputSignInPhoneDTO, OutputVerifyDTO, SignInEmailDTO, SignInPhoneDTO } from "./{{kebabCase name}}.service";
 
 
 @Service()
 @OpenAPI({
-    tags: ["{{kebabcase}}"],
+    tags: ["{{kebabCase name}}"],
 })
-@JsonController("{{kebabcase}}")
+@JsonController("{{kebabCase name}}")
 export class {{className}}Controller {
     constructor(public authService: {{className}}Service) { }
 
